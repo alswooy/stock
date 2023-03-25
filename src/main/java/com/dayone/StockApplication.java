ce45ae18e1c13a280d13d7f9694065c9590c6c90
alswooy <jjang990312@naver.com>
2023-03-25 13:34:04 +0900

    스케쥴링중

--- src/main/java/com/dayone/StockApplication.java
+++ src/main/java/com/dayone/StockApplication.java
@@ -2,13 +2,25 @@ package com.dayone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
+@EnableScheduling
 public class StockApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StockApplication.class, args);
+//		SpringApplication.run(StockApplication.class, args);
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("hello -> "+i);
+			try{
+				Thread.sleep(1000);
+			}catch (InterruptedException e){
+				throw new RuntimeException(e);
+			}
+
+		}
 
 	}
 
